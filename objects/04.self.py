10417645e6eb5f6b0258624b6a20fefe2f17eb63
Abel <abel.valverde041116@gmail.com>
2022-09-26 10:55:55 -0300

    obgetivo self

--- objects/04.self.py
+++ objects/04.self.py
@@ -3,3 +3,23 @@
 # palabra reservada class
 # 
 
+first_dict = {
+  "name": "student",
+  "position": "Fullstack Developer",
+  "skills": ["Python", "Git", "HTML","CSS","Javascript"]
+}
+
+class student:
+    def __init__(self,name,position,skills):
+        self.name = name
+        self.position = position
+        self.skills = skills
+    def say_name(self):
+        print("mi nombre es",self.name,"mi cargo es",self.position,"mis habilidades son",self.skills)
+
+Alice = student("Alice","Fullstack Developer",["Python", "Git", "Html","CSS","javastrip"])
+Bob_el_chef = student("Bob el chef","kitchen helper",["pastry chef international and cake shop"])
+Alice.say_name()
+Bob_el_chef.say_name()
+Abel = student("Abel","futbolista",["juego de centro miron"])
+Abel.say_name()
